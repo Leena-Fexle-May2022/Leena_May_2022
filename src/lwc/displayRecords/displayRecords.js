87c2183fb55fe859ae86f6a05d6b01a53add1888
Leena-Fexle-May2022 <105698555+Leena-Fexle-May2022@users.noreply.github.com>
2022-09-15 18:01:54 +0530

    updated

--- src/lwc/displayRecords/displayRecords.js
+++ src/lwc/displayRecords/displayRecords.js
@@ -13,22 +13,30 @@ export default class DisplayRecords extends LightningElement {
     @track itemList=[];
     @track value;
     view=false;
-    viewTable=false;
+    viewConTable=false;
+    viewCaseTable=false;
+    viewOppTable=false;
     @track idAccount;
     column=[{fieldName:'Name', label:'Name'},{fieldName:'Id', label:'Id'}];
     caseColumn=[{fieldName:'Id', label:'Id'},{fieldName:'CaseNumber', label:'Case Number'}];
     handleClick(event){
-        this.view=true;
+        this.viewConTable=false;
+        this.viewCaseTable=false;
+        this.viewOppTable=false;
         var inp = this.template.querySelector("lightning-input");
+        console.log('OUTPUT inp.value : ',inp.value);
         this.nameAccount = inp.value;
         getAccountList({input : this.nameAccount})
         .then(result => {
+            if(result.length !== 0){
+                this.view=true;
+            }
             let listAccounts = result;
             this.itemList=[];
             for(var i=0;i<listAccounts.length;i++) {
                 this.itemList.push({label: listAccounts[i].Name, value: listAccounts[i].Id});
             }
-            this.data = this.itemList;
+            this.data = this.itemList; 
         });
     }
     get optionsAccount() {
@@ -42,13 +50,22 @@ export default class DisplayRecords extends LightningElement {
     sequentialRecords(){
         getContact({accId : this.idAccount})
         .then((result1) => {
+            if(result1.length !== 0){
+                this.viewConTable=true;
+            }
             this.listContact = result1;
             getOpportunity({accId : this.idAccount})
             .then((result2) => {
+                if(result2.length !== 0){
+                    this.viewOppTable=true;
+                } 
                 this.listOpp = result2;
             })
             getCases({accId : this.idAccount})
             .then((result3)=>{
+                if(result3.length !== 0){
+                    this.viewCaseTable=true;
+                }
                 this.listCase = result3;
             })
         })
